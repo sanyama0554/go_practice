537c6264d8b271431ac1b7dd5d776026a3aa29c4
sanyama0554 <yuma.yamasaki554@gmail.com>
2026-06-17 15:24:21 +0900

    add interface.go

---
+++ interface.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type Printable interface {
+	ToString() string
+}
+
+func PrintOut(p Printable) {
+	fmt.Println(p.ToString())
+}
+
+type Person struct {
+	name string
+}
+
+func (p Person) ToString() string {
+	return p.name
+}
+
+type Book struct {
+	title string
+}
+
+func (b Book) ToString() string {
+	return b.title
+}
+
+func main() {
+	a1 := Person { name: "Alice" }
+	a2 := Book { title: "Go Programming" }
+	PrintOut(a1)
+	PrintOut(a2)
+}
